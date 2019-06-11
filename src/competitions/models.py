6f69c9600b28449f9a5f603e429d4a93949ece5d
Ivan Chavero <ichavero@chavero.com.mx>
2019-06-11 14:13:22 -0500

    Fix default names for models

--- src/competitions/models.py
+++ src/competitions/models.py
@@ -40,13 +40,13 @@ class ContestParticipant(models.Model):
     user    = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     role    = models.ForeignKey(Role, on_delete=models.DO_NOTHING)
     def __str__(self):
-        return self.name
+        return "%s at %s (%s)" % (self.user, self.contest, self.role)
     
 class ContestItem(models.Model):
     contest = models.ForeignKey(Contest, on_delete=models.DO_NOTHING)
     item    = models.ForeignKey(Item, on_delete=models.DO_NOTHING)
     def __str__(self):
-        return self.contest.name
+        return self.item.name
 
 class ContestTable(models.Model):
     contest = models.ForeignKey(Contest, on_delete=models.DO_NOTHING)
@@ -58,5 +58,5 @@ class ContestTableItem(models.Model):
     table = models.ForeignKey(ContestTable, on_delete=models.DO_NOTHING)
     item  = models.ForeignKey(ContestItem, on_delete=models.DO_NOTHING)
     def __str__(self):
-        return self.item.name
+        return self.item.item.name
 
