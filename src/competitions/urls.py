98e693a509e73f4c3274764db8f32d80ef0fb6fc
Ivan Chavero <ichavero@chavero.com.mx>
2019-06-13 15:51:23 -0500

    Fix urls for contests

--- src/competitions/urls.py
+++ src/competitions/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:contest_id>/', views.detail, name='detail'),
-    path('<int:contest_id>/scoresheet', views.scoresheet, name='scoresheet'),
+    #path('<int:contest_id>/scoresheet', views.scoresheet, name='scoresheet'),
+    path('<int:contest_id>/score', views.score, name='score'),
+    path('table/<int:table_id>', views.table, name='table'),
+    path('table/<int:table_id>/item/scoresheet/<int:item_id>', views.scoresheet, name='scoresheet'),
 ]
