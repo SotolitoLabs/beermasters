211d10a0286d4bfb86f776e36c17d4af72860a48
Ivan Chavero <ichavero@chavero.com.mx>
2019-06-14 15:02:29 -0500

    Add argument to start script

--- src/podman_run.sh
+++ src/podman_run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-podman run --network="host" -p 127.0.0.1:8000:8000 --rm -ti -v `pwd`:/code --name test-django sotolito/django /code/src/manage.py runserver 0:8000
+CONTAINER="test-django"
+
+if [[ ${1} != "" ]]; then
+    CONTAINER="$1"
+fi
+
+podman run --network="host" -p 127.0.0.1:8000:8000 --rm -ti -v `pwd`:/code --name $CONTAINER sotolito/django /code/src/manage.py runserver 0:8000
