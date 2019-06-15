49610f4005d168211b06d45232d9e36d0bdc7fde
Ivan Chavero <ichavero@chavero.com.mx>
2019-06-15 18:59:30 -0500

    Fix views for anoynimous users

--- src/competitions/views.py
+++ src/competitions/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import (Contest, ContestParticipant, ContestTable, 
-	ContestTableItem, Item)
+    ContestTableItem, Item)
 from django.template import loader
 
 # Create your views here.
@@ -18,27 +18,34 @@ def detail(request, contest_id):
     cps = [request.user]
     contest = Contest.objects.get(pk=contest_id)
     if request.user.id != None:
-    	cps = ContestParticipant.objects.filter(contest=contest_id).filter(user=request.user)
+        cps = ContestParticipant.objects.filter(
+        contest=contest_id).filter(user=request.user)
     cpts = ContestTable.objects.filter(contest=contest_id)
     return render(request, 'contest_detail.html', 
             {'contest': contest, 'participant': cps[0], 'tables': cpts})
 
 def scoresheet(request, table_id, item_id):
+    cps = [request.user]
     table = ContestTable.objects.get(pk=table_id)
+    if request.user.id != None:
+        cps = ContestParticipant.objects.filter(
+        contest=table.contest.id).filter(user=request.user)
     item = Item.objects.get(pk=item_id)
-    cps = ContestParticipant.objects.filter(contest=table.contest.id).filter(user=request.user)
     contest = Contest.objects.get(pk=table.contest.id)
     return render(request, 'score_sheet.html', {'participant': cps[0], 'contest': contest,
-	'table': table, 'contest': contest, 'item': item})
+    'table': table, 'contest': contest, 'item': item})
 
 def score(request, contest_id):
     contest = Contest.objects.get(pk=contest_id)
     return HttpResponse("Scoring beer %s category %s contest %s" % (1, 1, contest.id))
 
 def table(request, table_id):
+    cps = [request.user]
     table = ContestTable.objects.get(pk=table_id)
+    if request.user.id != None:
+        cps = ContestParticipant.objects.filter(
+        contest=table.contest.id).filter(user=request.user)
     items = ContestTableItem.objects.filter(table=table_id)
-    cps = ContestParticipant.objects.filter(contest=table.contest.id).filter(user=request.user)
     return render(request, 'table_detail.html', {'user': request.user,
-	'items': items, 'table': table, 'participant': cps[0]})
+    'items': items, 'table': table, 'participant': cps[0]})
 
