de593e8a249bdc2a5202bbb8874b1d1b423b8fd9
Ivan Chavero <ichavero@chavero.com.mx>
2019-06-16 15:45:38 -0500

    Fix score url

--- src/competitions/urls.py
+++ src/competitions/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+# TODO: fix URL's to make semantic sense
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:contest_id>/', views.detail, name='detail'),
     #path('<int:contest_id>/scoresheet', views.scoresheet, name='scoresheet'),
-    path('<int:contest_id>/score', views.score, name='score'),
+    path('<int:table_item_id>/score', views.score, name='score'),
     path('table/<int:table_id>', views.table, name='table'),
     path('table/<int:table_id>/item/scoresheet/<int:item_id>', views.scoresheet, name='scoresheet'),
 ]
