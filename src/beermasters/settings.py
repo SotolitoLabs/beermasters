d7a57b738b952ffb401e45124497910041a81e26
Ivan Chavero <ichavero@chavero.com.mx>
2019-07-07 13:11:44 -0500

    Fix login redirect url

--- src/beermasters/settings.py
+++ src/beermasters/settings.py
@@ -119,8 +119,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-LOGIN_REDIRECT_URL = '/competitions'
+LOGIN_REDIRECT_URL = '/competitions/'
 LOGOUT_REDIRECT_URL = '/competitions/'
+ 
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
