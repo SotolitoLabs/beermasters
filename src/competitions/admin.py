a61ed8bec2eeabee8b965980d3a16942dd2e08a7
Ivan Chavero <ichavero@chavero.com.mx>
2019-07-08 20:12:36 -0500

    Add styles and categories to admin

--- src/competitions/admin.py
+++ src/competitions/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import  (Brand, Item, Contest, Role, ContestParticipant,
   ContestItem, ContestTable, ContestTableItem, Aroma, Apperance, Flavor, 
   Mouthfeel, DescriptorDefinition, ContestScoreSheet, 
-  ContestScoreSheetDescriptor, ContestCategory)
+  ContestScoreSheetDescriptor, ContestCategory, BJCPcategory, BJCPstyle)
 
 # Register your models here.
 
@@ -22,3 +22,5 @@ admin.site.register(ContestItem)
 admin.site.register(ContestTable)
 admin.site.register(ContestTableItem)
 admin.site.register(ContestCategory)
+admin.site.register(BJCPcategory)
+admin.site.register(BJCPstyle)
