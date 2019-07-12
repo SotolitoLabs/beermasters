cefc1c16d398983fae8ffee51d4087fba452cac2
Ivan Chavero <ichavero@chavero.com.mx>
2019-07-12 18:32:21 -0500

    Add root redirect

--- src/beermasters/urls.py
+++ src/beermasters/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from competitions.views import index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('competitions/', include('competitions.urls')),
     path('admin/', admin.site.urls),
     # User management
