c9232604a1fd152b48bf3ebc058ae577fb2eb864
Ivan Chavero <ichavero@chavero.com.mx>
2019-08-19 20:25:49 -0500

    Add elements urls

--- src/competitions/urls.py
+++ src/competitions/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path('table/<int:table_id>/item/scoresheet/<int:item_id>', views.scoresheet, name='scoresheet'),
     path('register/', views.register, name='register'),
     path('profile/<int:user_id>', views.profile, name='profile'),
+    path('brewery/<int:brewery_id>', views.brewery_detail, name='brewery_detail'),
+    path('brewery/add', views.brewery, name='brewery'),
+    path('breweries/', views.breweries, name='breweries'),
+    path('beer/<int:beer_id>', views.beer_detail, name='beer_detail'),
+    #path('beer/add', views.brewery, name='brewery'),
 ]
