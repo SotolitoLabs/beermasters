172b3517ae1a8abeefefdf144f9c66abf29d8ece
Ivan Chavero <ichavero@chavero.com.mx>
2019-08-25 23:41:58 -0500

    Add arguments to postgres start script

--- src/postgres_up.sh
+++ src/postgres_up.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-podman run --name bm_postgres --network="host" -p 127.0.0.1:5432:5432 -e POSTGRES_PASSWORD=anygivenpassword -d postgres
+PASS="anygivenpassword"
+
+if [[ "${1}" != "" ]]; then
+    PASS=$1
+fi
+
+podman run --name bm_postgres --network="host" -p 127.0.0.1:5432:5432 -e POSTGRES_PASSWORD=${PASS} -d postgres
