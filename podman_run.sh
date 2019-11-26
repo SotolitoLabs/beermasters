9397ba9071e36d212be32c6eda7ebbbb7bcc151b
Ivan Chavero <ichavero@chavero.com.mx>
2019-11-26 15:50:43 -0600

    Format podman command

--- podman_run.sh
+++ podman_run.sh
@@ -6,4 +6,7 @@ if [[ ${1} != "" ]]; then
     CONTAINER="$1"
 fi
 
-podman run --network="host" -p 127.0.0.1:8000:8000 --rm -ti -v `pwd`:/code --name $CONTAINER sotolito/django /code/src/manage.py runserver 0:8000
+podman run --network="host" -p 127.0.0.1:8000:8000 --rm -ti -v `pwd`:/code \
+        --name $CONTAINER \
+        sotolito/django-prod \
+        /code/src/manage.py runserver 0:8000
